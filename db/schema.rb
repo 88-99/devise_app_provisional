7d1e793a7f8ece5f81c8443de5103dbde69e27b2
KW <kw@KWnoMacBook-Pro.local>
2022-01-06 20:04:54 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_03_044705) do
+ActiveRecord::Schema.define(version: 2022_01_01_023543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,37 +31,6 @@ ActiveRecord::Schema.define(version: 2022_01_03_044705) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
-  create_table "companies", force: :cascade do |t|
-    t.string "name"
-    t.string "phone_number"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "groupings", force: :cascade do |t|
-    t.bigint "company_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["company_id"], name: "index_groupings_on_company_id"
-    t.index ["user_id"], name: "index_groupings_on_user_id"
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.bigint "code", null: false
-    t.string "name", null: false
-    t.text "remarks"
-    t.integer "regular_price"
-    t.decimal "selling_price"
-    t.decimal "cost_price"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["code"], name: "index_products_on_code", unique: true
-    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -88,7 +57,4 @@ ActiveRecord::Schema.define(version: 2022_01_03_044705) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "groupings", "companies"
-  add_foreign_key "groupings", "users"
-  add_foreign_key "products", "users"
 end
